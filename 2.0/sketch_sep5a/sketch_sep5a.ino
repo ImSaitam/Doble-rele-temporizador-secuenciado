e0edf93eea6464c795cd0ad28e97e1fdc259d49f
ImSaitam <matutedesanto@gmail.com>
2025-09-08 22:49:21 -0300

    Final fix

--- 2.0/sketch_sep5a/sketch_sep5a.ino
+++ 2.0/sketch_sep5a/sketch_sep5a.ino
@@ -1,26 +1,27 @@
-int piston1 = 2; // Piston 1 = D2
-int piston2 = 3; // Piston 2 = D3
-int switchPin = 4;
-int switchPiston1 = 10; // Switch Piston 1 = D10
-int switchPiston2 = 9; // Switch Piston 2 = D9
-int pistonPrendido = 0;  // 0 = ninguno, 1 = piston1, 2 = piston2 
+int piston1 = 5; // D5
+int piston2 = 3; // D3
+int switchPin = 4; // D4
+int switchPiston1 = 12; // D12
+int switchPiston2 = 13; // D13
+int pistonPrendido = 0;  // 0 = ninguno, 1 = piston1, 2 = piston2
 bool sistemaActivo;
 bool primeraVez; 
 bool apagadoPendiente = false;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(piston1, OUTPUT);
   pinMode(piston2, OUTPUT);
   pinMode(switchPin, INPUT_PULLUP);
   pinMode(switchPiston1, INPUT_PULLUP);
   pinMode(switchPiston2, INPUT_PULLUP);
+  digitalWrite(piston1, HIGH);
+  digitalWrite(piston2, HIGH);
 
   primeraVez = true;
 }
 
 void loop() {
-  sistemaActivo = (digitalRead(switchPin) == HIGH);
+  sistemaActivo = (digitalRead(switchPin) == LOW);
 
   int p0 = analogRead(A0);
   int p1 = analogRead(A1);
@@ -38,10 +39,10 @@ void loop() {
   // --- Arranque inicial ---
   if (primeraVez && sistemaActivo) {
     if (estadoSwitch1 && estadoSwitch2) {
-      digitalWrite(piston1, HIGH);
+      digitalWrite(piston1, LOW);
       pistonPrendido = 1;
       delay(tp1);
-      digitalWrite(piston1, LOW);
+      digitalWrite(piston1, HIGH);
       primeraVez = false;
     }
     return;
@@ -49,35 +50,32 @@ void loop() {
 
   // --- Funcionamiento normal ---
   if (pistonPrendido == 1 && estadoSwitch1 && estadoSwitch2) {
-    digitalWrite(piston1, LOW);
-    digitalWrite(piston2, HIGH);
+    digitalWrite(piston1, HIGH);
+    digitalWrite(piston2, LOW);
     delay(tp2);
-    digitalWrite(piston1, LOW);
-    
+    digitalWrite(piston2, HIGH);
+
     if (apagadoPendiente) {
-      digitalWrite(piston2, LOW);
       apagadoPendiente = false;
       primeraVez = true;
       pistonPrendido = 0;
       return;
     }
 
-    digitalWrite(piston2, LOW);
     pistonPrendido = 2;
   }
   else if (pistonPrendido == 2 && estadoSwitch1 && estadoSwitch2) {
-    digitalWrite(piston1, HIGH);
-    delay(tp1);
-
-    if (apagadoPendiente) {
-      digitalWrite(piston1, LOW);
-      apagadoPendiente = false;
-      primeraVez = true;
+    if (!sistemaActivo) {
       pistonPrendido = 0;
+      primeraVez = true;
+      apagadoPendiente = false;
       return;
     }
 
     digitalWrite(piston1, LOW);
+    delay(tp1);
+    digitalWrite(piston1, HIGH);
+
     pistonPrendido = 1;
   }
 }
